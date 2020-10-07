c6a72c6b470832511f1536557686792d0e9e0834
Paris Dolfman <parisdolfman@gmail.com>
2020-10-07 19:10:31 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,21 +20,25 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
+
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database
@@ -58,7 +62,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
